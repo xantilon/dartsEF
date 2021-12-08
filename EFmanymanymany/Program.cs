0d334a63f904e8d847313d70e0ecd8256f4b1f28
Edgar Holzke <edgar.holzke@hamann-software.de>
2021-12-08 23:49:05 +0100

    add data

--- EFmanymanymany/Program.cs
+++ EFmanymanymany/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EFmanymanymany
 {
@@ -7,6 +9,37 @@ namespace EFmanymanymany
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using (var db = new DartsContext())
+            {
+                db.Players.AddRange(new List<Player>{
+                            new Player { Name = "Sepp" },
+                            new Player { Name = "Hanna" },
+                            new Player { Name="Susanne"}
+                        }
+                    );
+
+                db.SaveChanges();
+                db.Games.Add(new Game
+                {
+                    Positions = new List<Position>
+                    {
+                        new Position { Pos=1, Player = db.Players.First(p=>p.Name == "Susanne") },
+                        new Position { Pos=2, Player = db.Players.First(p=>p.Name == "Hanna") }
+                    }
+                });
+
+                db.Games.Add(new Game
+                {
+                    Positions = new List<Position>
+                    {
+                        new Position { Pos = 1, Player = db.Players.First(p=>p.Name == "Sepp") },
+                        new Position { Pos = 2, Player = db.Players.First(p=>p.Name == "Hanna") }
+                    }
+                });
+
+
+                db.SaveChanges();
+            }
         }
     }
 }
